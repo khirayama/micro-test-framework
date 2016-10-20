448ebdcd41844db5b578d0e0b036825ab4a68337
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-10-21 08:40:29 +0900

    Implement deepEqual

--- src/assert.js
+++ src/assert.js
@@ -1,23 +1,56 @@
-export function assert(value, expected) {
+export function assert(actual, expected) {
 }
 
-export function _equal(value, expected) {
-  if (value === expected) {
+export function _equal(actual, expected) {
+  if (actual === expected) {
     return true;
   }
   return false;
 }
 
-export function equal(value, expected) {
-  if (!_equal(value, expected)) {
+export function equal(actual, expected) {
+  if (!_equal(actual, expected)) {
     throw 'error';
   } else {
     return true;
   }
 }
 
-export function _deepEqual(value, expected) {
+export function _objEquiv(a, b) {
+  const ka = Object.keys(a);
+  const kb = Object.keys(b);
+
+  if (ka.length !== kb.length) {
+    return false;
+  }
+  ka.sort();
+  kb.sort();
+  for (let i = ka.length - 1; i >= 0; i--) {
+    let key = ka[i];
+    if (!_deepEqual(a[key], b[key])) {
+      return false;
+    }
+  }
+  return typeof a === typeof b;
 }
 
-export function deepEqual(value, expected) {
+export function _deepEqual(actual, expected) {
+  if (actual instanceof Date && expected instanceof Date) {
+    return actual.getTime() === expected.getTime();
+  } else if (
+    !actual || !expected ||
+    typeof actual != 'object' && typeof expected != 'object'
+  ) {
+    return actual === expected;
+  } else {
+    return _objEquiv(actual, expected);
+  }
+}
+
+export function deepEqual(actual, expected) {
+  if (!_deepEqual(actual, expected)) {
+    throw 'error';
+  } else {
+    return true;
+  }
 }
